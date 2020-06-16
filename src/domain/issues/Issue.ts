7c0ee85f51bbeaf80db2fe250a58798d86c07005
ModellbahnFreak <freak.mdellbahn@gmail.com>
2020-06-16 03:21:43 +0200

    Autoformat

--- src/domain/issues/Issue.ts
+++ src/domain/issues/Issue.ts
@@ -1,22 +1,22 @@
-import {User} from "../users/User";
-import {Component} from "../components/Component";
+import { User } from "../users/User";
+import { Component } from "../components/Component";
 import { IssueComment } from "./IssueComment";
 
 export class Issue {
 
-    private readonly _creator : User;
+    private readonly _creator: User;
 
-    private _comments : IssueComment[]
+    private _comments: IssueComment[]
 
-    private readonly _creationDate : Date;
+    private readonly _creationDate: Date;
 
-    private readonly _component : Component;
+    private readonly _component: Component;
 
-    private readonly _id : string;
+    private readonly _id: string;
 
-    private _linkedIssues : string[]
+    private _linkedIssues: string[]
 
-    public constructor (component : Component, id : string, creator : User, creationDate : Date) {
+    public constructor(component: Component, id: string, creator: User, creationDate: Date) {
         this._component = component;
         this._id = id;
         this._creator = creator;
@@ -27,31 +27,31 @@ export class Issue {
 
     private refreshLinkedIssues() {
         this._linkedIssues = []
-        this._comments.forEach(comment => )
+        //this._comments.forEach(comment => )
     }
 
-    public createComment(asUser : User, body : string) {
+    public createComment(asUser: User, body: string) {
 
-        
+
     }
 
     /**
      * add an alredy existing IssueComment to this Issue
      * @param comment 
      */
-    public addComment(comment : IssueComment) {
+    public addComment(comment: IssueComment) {
         this.refreshLinkedIssues();
     }
 
-    public get creator() : User {
+    public get creator(): User {
         return this._creator;
     }
 
-    public get comments() : IssueComment[] {
+    public get comments(): IssueComment[] {
         return this._comments;
     }
 
-    public get creationDate() : Date {
+    public get creationDate(): Date {
         return this._creationDate;
     }
 }
--- src/domain/users/User.ts
+++ src/domain/users/User.ts
@@ -1,26 +1,26 @@
-import {Client} from "pg"
+import { Client } from "pg"
 import { DatabaseElement } from "../DatabaseElement";
 
 export class User extends DatabaseElement {
-    private _userName : string;
+    private _userName: string;
 
-    private _password : string;
+    private _password: string;
 
-    private constructor(client : Client, id : string) {
+    private constructor(client: Client, id: string) {
         super(client, id);
         this._userName = "";
         this._password = "";
     }
 
-    public get userName() : string {
+    public get userName(): string {
         return this._userName;
     }
 
-    public set userName(userName : string) {
+    public set userName(userName: string) {
         this._userName = userName;
     }
 
-    public static byUserName() : User | undefined {
+    public static byUserName(username: string): User | undefined {
         return undefined;
     }
 
