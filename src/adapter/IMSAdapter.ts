13699d12883fcf16864d111bcc8cdb61b2a7de50
ModellbahnFreak <freak.mdellbahn@gmail.com>
2020-06-17 02:26:12 +0200

    Started ims adapter

--- src/adapter/IMSAdapter.ts
+++ src/adapter/IMSAdapter.ts
@@ -1,3 +1,10 @@
+import { Issue } from "../domain/issues/Issue";
+import { IssueComment } from "../domain/issues/IssueComment";
+
 export interface IMSAdapter {
-    
+
+    getIssues(): Array<Issue>;
+    getComments(issue: Issue): Array<IssueComment>;
+
+
 }
--- src/adapter/IMSCredential.ts
+++ src/adapter/IMSCredential.ts
@@ -1,21 +1,21 @@
 import { IMSType } from "./IMSType";
 import { TypeNode, Token } from "graphql";
-import { GitHubCredential } from "./GitHubCredential";
+import { GitHubCredential } from "./github/GitHubCredential";
 import { IMSCredentialInfo } from "./IMSCredentialInfo";
 import { GithubCredentialInfo } from "./GithubCredentialInfo";
 
 export class IMSCredential {
-    private readonly _info : IMSCredentialInfo;
+    private readonly _info: IMSCredentialInfo;
 
-    protected constructor(info : IMSCredentialInfo) {
+    protected constructor(info: IMSCredentialInfo) {
         this._info = info;
     }
 
-    public get info() : IMSCredentialInfo {
+    public get info(): IMSCredentialInfo {
         return this._info;
     }
 
-    public static parse(info: IMSCredentialInfo, data : string) : IMSCredential {
+    public static parse(info: IMSCredentialInfo, data: string): IMSCredential {
         switch (info.type) {
             case IMSType.GitHub:
                 return new GitHubCredential(info as GithubCredentialInfo, data);
