b16969e332f202317dce04cd1a7df1eefecf2cfb
ModellbahnFreak <freak.mdellbahn@gmail.com>
2020-06-17 04:11:23 +0200

    Implemented getIssues and getComments on GithubAdaptor

--- src/adapter/IMSAdapter.ts
+++ src/adapter/IMSAdapter.ts
@@ -1,10 +1,11 @@
 import { Issue } from "../domain/issues/Issue";
 import { IssueComment } from "../domain/issues/IssueComment";
+import { User } from "../domain/users/User";
 
 export interface IMSAdapter {
 
-    getIssues(): Array<Issue>;
-    getComments(issue: Issue): Array<IssueComment>;
+    getIssues(user: User): Promise<Array<Issue>>;
+    getComments(issue: Issue, user: User): Promise<Array<IssueComment>>;
 
 
 }
