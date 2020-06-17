3caf02423aed9cd455179d17f2cc49e7501f87ae
nk-coding <niklask.coding@gmail.com>
2020-06-17 05:56:11 +0200

    fixed typo

--- src/adapter/IMSInfoProvider.ts
+++ src/adapter/IMSInfoProvider.ts
@@ -7,7 +7,7 @@ import { IMSType } from "./IMSType";
 export class IMSInfoProvider{
     public static async load(client : DBClient, id : BigInt) : Promise<IMSInfo> {
         const pq = client.client;
-        return pq.query("SELECT type, data FROM issue_managemant_systems WHERE id=$1::bigint;", [id]).then(result => {
+        return pq.query("SELECT type, data FROM issue_management_systems WHERE id=$1::bigint;", [id]).then(result => {
             switch (IMSType[result.rows[0]["type"] as keyof typeof IMSType]) {
                 case IMSType.GitHub:
                     return new GitHubIMSInfo(client, id, result.rows[0]["data"]);
