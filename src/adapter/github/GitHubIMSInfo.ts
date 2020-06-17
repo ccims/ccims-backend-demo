1238ec86b35e6b483aa67ce96f3a73e28c05a83a
ModellbahnFreak <freak.mdellbahn@gmail.com>
2020-06-17 05:05:55 +0200

    Added client id to github ims info

--- src/adapter/github/GitHubIMSInfo.ts
+++ src/adapter/github/GitHubIMSInfo.ts
@@ -5,13 +5,28 @@ import { DBClient } from "../../domain/DBClient";
 export class GitHubIMSInfo extends IMSInfo {
 
     private readonly _endpoint: string;
+    private readonly _clientId: string;
 
-    public constructor(client: DBClient, id: BigInt, endpoint: string) {
+    public constructor(client: DBClient, id: BigInt, data: string) {
         super(client, id, IMSType.GitHub);
-        this._endpoint = endpoint;
+        const dataParsed = JSON.parse(data) as GithubImsData;
+        if (!GitHubIMSInfo.isGithubData(dataParsed)) {
+            throw new Error("The given data wasn''t github data");
+        }
+        this._endpoint = dataParsed.endpoint;
+        this._clientId = dataParsed.clientId;
     }
 
     public get endpoint() {
         return this._endpoint;
     }
+
+    private static isGithubData(data: GithubImsData): boolean {
+        return typeof data.clientId === "string" && typeof data.endpoint === "string";
+    }
+}
+
+interface GithubImsData {
+    endpoint: string;
+    clientId: string;
 }
