3411b7ac766258f9193301cad6c2f0b331d1a753
nk-coding <niklask.coding@gmail.com>
2020-06-23 22:43:20 +0200

    throw error if no or too many IMSInfos are found in the database

--- src/adapter/IMSInfoProvider.ts
+++ src/adapter/IMSInfoProvider.ts
@@ -7,11 +7,16 @@ import { IMSType } from "./IMSType";
 export class IMSInfoProvider{
     public static async _load(client : DBClient, id : string) : Promise<IMSInfo> {
         const pq = client.client;
-        return pq.query("SELECT type, data FROM issue_management_systems WHERE id=$1;", [id]).then(result => {
-            switch (IMSType[result.rows[0]["type"] as keyof typeof IMSType]) {
-                case IMSType.GitHub:
-                    return new GitHubIMSInfo(client, id, result.rows[0]["data"]);
+        return pq.query("SELECT type, data FROM issue_management_systems WHERE id=$1;", [id]).then(res => {
+            if (res.rowCount !== 1) {
+                throw new Error("illegal number of IMSInfos found");
+            } else {
+                switch (IMSType[res.rows[0]["type"] as keyof typeof IMSType]) {
+                    case IMSType.GitHub:
+                        return new GitHubIMSInfo(client, id, res.rows[0]["data"]);
+                }
             }
+            
         });
     }
 } 
